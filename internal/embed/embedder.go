d720babf6680693904526efd13d70fc5e1d6c706
stonebanks <github.com.u4jw4@passmail.net>
2026-08-14 11:25:49 -0400

    feat: define interfaces for embedding and storing

--- internal/embed/embedder.go
+++ internal/embed/embedder.go
@@ -2,6 +2,11 @@ package embed
 
 import "context"
 
+type CommitMessageEmbeddingResult struct {
+	Vector []float32
+	Model  string
+}
+
 type Embedder interface {
-	Embed(ctx context.Context, str string)
+	Embed(ctx context.Context, str string) (CommitMessageEmbeddingResult, error)
 }
--- internal/store/repository.go
+++ internal/store/repository.go
@@ -1,7 +1,45 @@
 package store
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type Repository struct {
 	Db *sql.DB
+}
+
+type Commit struct {
+	SHA            string
+	Body           string
+	AuthorName     string
+	AuthorEmail    string
+	AuthorDate     time.Time
+	CommitterName  string
+	CommitterEmail string
+	CommitterDate  time.Time
+}
+
+type EmbeddingSource string
+
+const (
+	CommitMessage EmbeddingSource = "message"
+	DiffSummary   EmbeddingSource = "diff_summary"
+)
+
+type Embedding struct {
+	SHA    string
+	Vector []float32
+	Model  string
+	Source EmbeddingSource
+}
+
+type EnrichedCommit struct {
+	Commit    Commit
+	Embedding Embedding
+}
+
+type Store interface {
+	SaveEnrichedCommit(ctx context.Context, commits []EnrichedCommit) error
 }
