04af7f9fc166be3ec707c8b007124371846be878
stonebanks <github.com.u4jw4@passmail.net>
2026-08-26 13:20:06 -0400

    feat: wiring the indexing process (save to sqlite yet to happen)

--- internal/cli/index.go
+++ internal/cli/index.go
@@ -1,7 +1,13 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/stonebanks/histquery/internal/embed/ollama"
+	"github.com/stonebanks/histquery/internal/indexer"
+	"github.com/stonebanks/histquery/internal/ingest/localgit"
+	"github.com/stonebanks/histquery/internal/store/sqlite"
 )
 
 var indexCmd = &cobra.Command{
@@ -15,5 +21,16 @@ func init() {
 }
 
 func runIndex(cmd *cobra.Command, args []string) error {
+	ctx := cmd.Context()
+
+	source := cmd.Context().Value(gitRepoKey).(*localgit.Source)
+	repo := cmd.Context().Value(sqliteRepoKey).(*sqlite.Repository)
+	embedder := ollama.New("")
+
+	idxr := indexer.New(source, embedder, repo)
+	if err := idxr.Run(ctx, &indexer.IndexerOptions{}); err != nil {
+		return fmt.Errorf("indexing: %w", err)
+	}
+
 	return nil
 }
